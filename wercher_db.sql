867c523ef010c05a8f134138b1643505b1076a89
Wyrmu <38562978+Wyrmu@users.noreply.github.com>
2019-12-09 21:56:15 +0800

    Latest DB update

--- wercher_db.sql
+++ wercher_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.2
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 24, 2019 at 09:28 PM
--- Server version: 10.1.34-MariaDB
--- PHP Version: 7.2.7
+-- Host: 127.0.0.1:3306
+-- Generation Time: Nov 24, 2019 at 08:18 PM
+-- Server version: 5.7.26
+-- PHP Version: 7.2.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -28,15 +28,14 @@ SET time_zone = "+00:00";
 -- Table structure for table `acad_history`
 --
 
-CREATE TABLE `acad_history` (
-  `Acad_No` int(11) NOT NULL,
-  `ApplicantID` varchar(255) DEFAULT NULL,
-  `Level` varchar(255) DEFAULT NULL,
-  `SchoolName` varchar(255) DEFAULT NULL,
-  `SchoolAddress` varchar(255) DEFAULT NULL,
+DROP TABLE IF EXISTS `acad_history`;
+CREATE TABLE IF NOT EXISTS `acad_history` (
+  `Acad_No` int(11) NOT NULL AUTO_INCREMENT,
+  `EmployeeID` varchar(255) DEFAULT NULL,
+  `Location` varchar(255) DEFAULT NULL,
   `DateStarted` varchar(255) DEFAULT NULL,
   `DateEnds` varchar(255) NOT NULL,
-  `HighDegree` varchar(255) DEFAULT NULL
+  PRIMARY KEY (`Acad_No`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -45,94 +44,20 @@ CREATE TABLE `acad_history` (
 -- Table structure for table `admin`
 --
 
-CREATE TABLE `admin` (
-  `AdminNo` int(11) NOT NULL,
-  `AdminLevel` varchar(255) DEFAULT NULL,
-  `Position` varchar(255) DEFAULT NULL,
-  `AdminID` varchar(255) DEFAULT NULL,
-  `Password` varchar(255) DEFAULT NULL,
-  `FirstName` varchar(255) DEFAULT NULL,
-  `MiddleInitial` varchar(255) DEFAULT NULL,
-  `LastName` varchar(255) DEFAULT NULL,
-  `Gender` varchar(255) DEFAULT NULL,
-  `DateAdded` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE IF NOT EXISTS `admin` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Access_ID` varchar(255) NOT NULL,
+  `Branch` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `admin`
 --
 
-INSERT INTO `admin` (`AdminNo`, `AdminLevel`, `Position`, `AdminID`, `Password`, `FirstName`, `MiddleInitial`, `LastName`, `Gender`, `DateAdded`) VALUES
-(7, 'Level_1', 'Developer', 'Dev-0001', '$2y$10$yJEJbZZiXlqaFAWZE08.geoEc3tfpRGkQyYVACpcy4ukOow.mESim', 'Romel', 'P', 'Cubelo', 'Male', '1573753020');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `applicants`
---
-
-CREATE TABLE `applicants` (
-  `ApplicantNo` int(11) NOT NULL,
-  `ApplicantImage` blob,
-  `ApplicantID` varchar(255) DEFAULT NULL,
-  `PositionDesired` varchar(255) DEFAULT NULL,
-  `SalaryExpected` varchar(255) DEFAULT NULL,
-  `LastName` varchar(255) DEFAULT NULL,
-  `FirstName` varchar(255) DEFAULT NULL,
-  `MiddleInitial` varchar(255) DEFAULT NULL,
-  `Gender` varchar(255) DEFAULT NULL,
-  `Age` varchar(255) DEFAULT NULL,
-  `Height` varchar(255) DEFAULT NULL,
-  `Weight` varchar(255) DEFAULT NULL,
-  `Religion` varchar(255) DEFAULT NULL,
-  `BirthDate` varchar(255) DEFAULT NULL,
-  `BirthPlace` varchar(255) DEFAULT NULL,
-  `Citizenship` varchar(255) DEFAULT NULL,
-  `CivilStatus` varchar(255) DEFAULT NULL,
-  `No_OfChildren` varchar(255) DEFAULT NULL,
-  `Address_Present` varchar(255) DEFAULT NULL,
-  `Address_Provincial` varchar(255) DEFAULT NULL,
-  `Address_Manila` varchar(255) DEFAULT NULL,
-  `Phone_No` varchar(255) DEFAULT NULL,
-  `SSS_No` varchar(255) DEFAULT NULL,
-  `EffectiveDateCoverage` varchar(255) DEFAULT NULL,
-  `ResidenceCertificateNo` varchar(255) DEFAULT NULL,
-  `Rcn_At` varchar(255) DEFAULT NULL,
-  `Rcn_On` varchar(255) DEFAULT NULL,
-  `TIN` varchar(255) DEFAULT NULL,
-  `TIN_At` varchar(255) DEFAULT NULL,
-  `TIN_On` varchar(255) DEFAULT NULL,
-  `Status` varchar(255) DEFAULT NULL,
-  `ClientEmployed` varchar(255) DEFAULT NULL,
-  `DateStarted` varchar(255) DEFAULT NULL,
-  `DateEnds` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `beneficiaries`
---
-
-CREATE TABLE `beneficiaries` (
-  `No` int(11) NOT NULL,
-  `ApplicationID` varchar(255) DEFAULT NULL,
-  `Name` varchar(255) DEFAULT NULL,
-  `Relationship` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `clients`
---
-
-CREATE TABLE `clients` (
-  `ClientID` int(11) NOT NULL,
-  `Name` varchar(255) DEFAULT NULL,
-  `Address` varchar(255) DEFAULT NULL,
-  `ContactNumber` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+INSERT INTO `admin` (`ID`, `Access_ID`, `Branch`) VALUES
+(1, 'master', '1');
 
 -- --------------------------------------------------------
 
@@ -140,8 +65,9 @@ CREATE TABLE `clients` (
 -- Table structure for table `employee`
 --
 
-CREATE TABLE `employee` (
-  `Employee_No` int(11) NOT NULL,
+DROP TABLE IF EXISTS `employee`;
+CREATE TABLE IF NOT EXISTS `employee` (
+  `Employee_No` int(11) NOT NULL AUTO_INCREMENT,
   `Employee_ID` varchar(255) NOT NULL,
   `EmployeeImage` blob,
   `EmploymentType` varchar(255) DEFAULT NULL,
@@ -161,167 +87,16 @@ CREATE TABLE `employee` (
   `ATM` varchar(255) DEFAULT NULL,
   `DateHired` varchar(255) DEFAULT NULL,
   `DateSeparated` varchar(255) DEFAULT NULL,
-  `Status` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `employment_record`
---
-
-CREATE TABLE `employment_record` (
-  `No` int(11) NOT NULL,
-  `ApplicantID` varchar(255) DEFAULT NULL,
-  `Name` varchar(255) DEFAULT NULL,
-  `Address` varchar(255) DEFAULT NULL,
-  `PeriodCovered` varchar(255) DEFAULT NULL,
-  `Position` varchar(255) DEFAULT NULL,
-  `Salary` varchar(255) DEFAULT NULL,
-  `CauseOfSeparation` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
+  `Status` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Employee_No`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
--- Table structure for table `machine_operated`
+-- Dumping data for table `employee`
 --
 
-CREATE TABLE `machine_operated` (
-  `No` int(11) NOT NULL,
-  `ApplicantID` varchar(255) DEFAULT NULL,
-  `MachineName` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `relatives`
---
-
-CREATE TABLE `relatives` (
-  `No` int(11) NOT NULL,
-  `ApplicantID` varchar(255) DEFAULT NULL,
-  `Relation` varchar(255) DEFAULT NULL,
-  `Name` varchar(255) DEFAULT NULL,
-  `Occupation` varchar(2555) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `acad_history`
---
-ALTER TABLE `acad_history`
-  ADD PRIMARY KEY (`Acad_No`);
-
---
--- Indexes for table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`AdminNo`);
-
---
--- Indexes for table `applicants`
---
-ALTER TABLE `applicants`
-  ADD PRIMARY KEY (`ApplicantNo`);
-
---
--- Indexes for table `beneficiaries`
---
-ALTER TABLE `beneficiaries`
-  ADD PRIMARY KEY (`No`);
-
---
--- Indexes for table `clients`
---
-ALTER TABLE `clients`
-  ADD PRIMARY KEY (`ClientID`);
-
---
--- Indexes for table `employee`
---
-ALTER TABLE `employee`
-  ADD PRIMARY KEY (`Employee_No`);
-
---
--- Indexes for table `employment_record`
---
-ALTER TABLE `employment_record`
-  ADD PRIMARY KEY (`No`);
-
---
--- Indexes for table `machine_operated`
---
-ALTER TABLE `machine_operated`
-  ADD PRIMARY KEY (`No`);
-
---
--- Indexes for table `relatives`
---
-ALTER TABLE `relatives`
-  ADD PRIMARY KEY (`No`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `acad_history`
---
-ALTER TABLE `acad_history`
-  MODIFY `Acad_No` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `admin`
---
-ALTER TABLE `admin`
-  MODIFY `AdminNo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT for table `applicants`
---
-ALTER TABLE `applicants`
-  MODIFY `ApplicantNo` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `beneficiaries`
---
-ALTER TABLE `beneficiaries`
-  MODIFY `No` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `clients`
---
-ALTER TABLE `clients`
-  MODIFY `ClientID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `employee`
---
-ALTER TABLE `employee`
-  MODIFY `Employee_No` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `employment_record`
---
-ALTER TABLE `employment_record`
-  MODIFY `No` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `machine_operated`
---
-ALTER TABLE `machine_operated`
-  MODIFY `No` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `relatives`
---
-ALTER TABLE `relatives`
-  MODIFY `No` int(11) NOT NULL AUTO_INCREMENT;
+INSERT INTO `employee` (`Employee_No`, `Employee_ID`, `EmployeeImage`, `EmploymentType`, `LastName`, `FirstName`, `MiddleInitial`, `Address`, `BirthDate`, `BirthPlace`, `Gender`, `MaritalStatus`, `ProjectAssigned`, `SSS`, `Philhealth`, `HDMF`, `TIN`, `ATM`, `DateHired`, `DateSeparated`, `Status`) VALUES
+(1, '00001-A', 0x68747470733a2f2f6c6f63616c6465762e746573742f48525f53797374656d2f75706c6f6164732f30303030312d412f31343062313465303739646338356337326339663032356362363764623536322e706e67, 'Employee', 'Dasdas', 'Dasdasd', 'Asdas', 'asdasdasdas', '2019-10-21', 'asdasdas', 'Female', 'Single', 'dasdada', 'sdadada', NULL, NULL, NULL, NULL, '2019-10-23', '2019-10-31', 'Active');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
