acf8639c867808bfdcb18a7a862d9c6c78fe3dd2
Romel Cubelo <romelcub@gmail.com>
2019-12-11 20:43:25 +0800

    Added latest db

--- wercher_db.sql
+++ wercher_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.8.2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Nov 24, 2019 at 08:18 PM
--- Server version: 5.7.26
--- PHP Version: 7.2.18
+-- Host: 127.0.0.1
+-- Generation Time: Dec 11, 2019 at 01:42 PM
+-- Server version: 10.1.34-MariaDB
+-- PHP Version: 7.2.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -28,15 +28,24 @@ SET time_zone = "+00:00";
 -- Table structure for table `acad_history`
 --
 
-DROP TABLE IF EXISTS `acad_history`;
-CREATE TABLE IF NOT EXISTS `acad_history` (
-  `Acad_No` int(11) NOT NULL AUTO_INCREMENT,
-  `EmployeeID` varchar(255) DEFAULT NULL,
-  `Location` varchar(255) DEFAULT NULL,
+CREATE TABLE `acad_history` (
+  `Acad_No` int(11) NOT NULL,
+  `ApplicantID` varchar(255) DEFAULT NULL,
+  `Level` varchar(255) DEFAULT NULL,
+  `SchoolName` varchar(255) DEFAULT NULL,
+  `SchoolAddress` varchar(255) DEFAULT NULL,
   `DateStarted` varchar(255) DEFAULT NULL,
   `DateEnds` varchar(255) NOT NULL,
-  PRIMARY KEY (`Acad_No`)
+  `HighDegree` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `acad_history`
+--
+
+INSERT INTO `acad_history` (`Acad_No`, `ApplicantID`, `Level`, `SchoolName`, `SchoolAddress`, `DateStarted`, `DateEnds`, `HighDegree`) VALUES
+(1, '00007-A', 'Other courses / Special Training', '5555555', '55555', '2015', '2050', 'Yes'),
+(2, '00008-A', 'High School', 'SAMPLE', 'SAMPLE', 'SAMP', 'SAMP', 'SAMPLE');
 
 -- --------------------------------------------------------
 
@@ -44,20 +53,142 @@ CREATE TABLE IF NOT EXISTS `acad_history` (
 -- Table structure for table `admin`
 --
 
-DROP TABLE IF EXISTS `admin`;
-CREATE TABLE IF NOT EXISTS `admin` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Access_ID` varchar(255) NOT NULL,
-  `Branch` varchar(255) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+CREATE TABLE `admin` (
+  `AdminNo` int(11) NOT NULL,
+  `AdminLevel` varchar(255) DEFAULT NULL,
+  `Position` varchar(255) DEFAULT NULL,
+  `AdminID` varchar(255) DEFAULT NULL,
+  `Password` varchar(255) DEFAULT NULL,
+  `FirstName` varchar(255) DEFAULT NULL,
+  `MiddleInitial` varchar(255) DEFAULT NULL,
+  `LastName` varchar(255) DEFAULT NULL,
+  `Gender` varchar(255) DEFAULT NULL,
+  `DateAdded` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `admin`
 --
 
-INSERT INTO `admin` (`ID`, `Access_ID`, `Branch`) VALUES
-(1, 'master', '1');
+INSERT INTO `admin` (`AdminNo`, `AdminLevel`, `Position`, `AdminID`, `Password`, `FirstName`, `MiddleInitial`, `LastName`, `Gender`, `DateAdded`) VALUES
+(7, 'Level_1', 'Developer', 'Dev-0001', '$2y$10$yJEJbZZiXlqaFAWZE08.geoEc3tfpRGkQyYVACpcy4ukOow.mESim', 'Romel', 'P', 'Cubelo', 'Male', '1573753020'),
+(8, 'Level_1', 'Developer', 'Dev-0002', '$2y$10$u4Dv7PQgGdDL1I/XPar15e3bhNv4RQKN..3LD3aBKsV8DWdmHIPuu', 'Tewi', 'I', 'Inaba', 'Female', '1575271925');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `applicants`
+--
+
+CREATE TABLE `applicants` (
+  `ApplicantNo` int(11) NOT NULL,
+  `ApplicantImage` blob,
+  `ApplicantID` varchar(255) DEFAULT NULL,
+  `PositionDesired` varchar(255) DEFAULT NULL,
+  `SalaryExpected` varchar(255) DEFAULT NULL,
+  `LastName` varchar(255) DEFAULT NULL,
+  `FirstName` varchar(255) DEFAULT NULL,
+  `MiddleInitial` varchar(255) DEFAULT NULL,
+  `Gender` varchar(255) DEFAULT NULL,
+  `Age` varchar(255) DEFAULT NULL,
+  `Height` varchar(255) DEFAULT NULL,
+  `Weight` varchar(255) DEFAULT NULL,
+  `Religion` varchar(255) DEFAULT NULL,
+  `BirthDate` varchar(255) DEFAULT NULL,
+  `BirthPlace` varchar(255) DEFAULT NULL,
+  `Citizenship` varchar(255) DEFAULT NULL,
+  `CivilStatus` varchar(255) DEFAULT NULL,
+  `No_OfChildren` varchar(255) DEFAULT NULL,
+  `Address_Present` varchar(255) DEFAULT NULL,
+  `Address_Provincial` varchar(255) DEFAULT NULL,
+  `Address_Manila` varchar(255) DEFAULT NULL,
+  `Phone_No` varchar(255) DEFAULT NULL,
+  `SSS_No` varchar(255) DEFAULT NULL,
+  `EffectiveDateCoverage` varchar(255) DEFAULT NULL,
+  `ResidenceCertificateNo` varchar(255) DEFAULT NULL,
+  `Rcn_At` varchar(255) DEFAULT NULL,
+  `Rcn_On` varchar(255) DEFAULT NULL,
+  `TIN` varchar(255) DEFAULT NULL,
+  `TIN_At` varchar(255) DEFAULT NULL,
+  `TIN_On` varchar(255) DEFAULT NULL,
+  `HDMF` varchar(255) DEFAULT NULL,
+  `HDMF_At` varchar(255) DEFAULT NULL,
+  `HDMF_On` varchar(255) DEFAULT NULL,
+  `PhilHealth` varchar(255) DEFAULT NULL,
+  `PhilHealth_At` varchar(255) DEFAULT NULL,
+  `PhilHealth_On` varchar(255) DEFAULT NULL,
+  `ATM_No` varchar(255) DEFAULT NULL,
+  `Status` varchar(255) DEFAULT NULL,
+  `ClientEmployed` varchar(255) DEFAULT NULL,
+  `DateStarted` varchar(255) DEFAULT NULL,
+  `DateEnds` varchar(255) DEFAULT NULL,
+  `AppliedOn` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `applicants`
+--
+
+INSERT INTO `applicants` (`ApplicantNo`, `ApplicantImage`, `ApplicantID`, `PositionDesired`, `SalaryExpected`, `LastName`, `FirstName`, `MiddleInitial`, `Gender`, `Age`, `Height`, `Weight`, `Religion`, `BirthDate`, `BirthPlace`, `Citizenship`, `CivilStatus`, `No_OfChildren`, `Address_Present`, `Address_Provincial`, `Address_Manila`, `Phone_No`, `SSS_No`, `EffectiveDateCoverage`, `ResidenceCertificateNo`, `Rcn_At`, `Rcn_On`, `TIN`, `TIN_At`, `TIN_On`, `HDMF`, `HDMF_At`, `HDMF_On`, `PhilHealth`, `PhilHealth_At`, `PhilHealth_On`, `ATM_No`, `Status`, `ClientEmployed`, `DateStarted`, `DateEnds`, `AppliedOn`) VALUES
+(1, 0x68747470733a2f2f6c6f63616c686f73742f63695f776572636865725f73797374656d2f75706c6f6164732f30303030312d412f6368726f6d655f4a7179474e53314973642e6a7067, '00001-A', 'Payroll Specialist', '50000', 'Haniyasushin', 'Keiki', 'H', 'Female', '9999', '5', '5', 'Self', '150015-12-05', 'Spirit World', '12345', 'Single', '500', '11111111', '222222222', '33333333', '123456789', '111111111', '2019-12-10', '1111111', '11111111111', '2019-12-05', '22222222222', '2222222222222', '2019-12-20', '', '', '', '', '', '', NULL, 'Employed', '2', '2019-12-02 09:34:52 PM', '2020-02-02 09:34:52 PM', NULL),
+(2, 0x68747470733a2f2f6c6f63616c686f73742f63695f776572636865725f73797374656d2f75706c6f6164732f30303030322d412f646c6c686f73745f6f544d3551453634556c2e6a7067, '00002-A', 'Office Staff', '80000', 'Inaba', 'Tewi', 'I', 'Female', '3000', '5', '5', '5555555', '10000-05-13', 'Eientei', '151461246', 'Single', '0', 'Eientei', 'Gensokyo', '12345', '1353141', '2456245134', '2019-12-12', '23423526246', 'A', '2019-12-13', '264622343256246', 'B', '2019-12-21', '', '', '', '', '', '', NULL, 'Expired', '1', '', '2020-02-02 08:56:06 PM', NULL),
+(3, 0x68747470733a2f2f6c6f63616c686f73742f63695f776572636865725f73797374656d2f75706c6f6164732f30303030332d412f6d6f7274792e6a7067, '00003-A', 'Office Staff', '500', 'Smith', 'Morty', 'S', 'Male', '10', '5', '5', 'Rick', '2007-12-01', 'Earth', 'Yes', 'Single', '0', '252452524', '524524524', '5245245245245', '25425245', '24525245', '2019-12-19', '245245245', '2452525245245', '2019-12-14', '245245', '24245245245245245245', '2020-01-20', '', '', '', '', '', '', NULL, 'Applicant', NULL, NULL, NULL, NULL),
+(4, 0x68747470733a2f2f6c6f63616c686f73742f63695f776572636865725f73797374656d2f75706c6f6164732f30303030342d412f446169796f757365692e6a7067, '00004-A', 'Office Staff', '8888888', 'Test', 'Test', 'Test', 'Male', '35', '531', '135', '2352561', '2019-12-18', '13513513', '1354156', 'Widowed', '5135413', '31515135', '135135', '135135135135135', '4156135', '153515', '2019-12-10', '135135', '51351313', '0135-12-05', '13513531', '5135135', '2019-12-13', '', '', '', '', '', '', NULL, 'Deleted', '1', '', '2020-05-02 08:59:04 PM', NULL),
+(5, 0x68747470733a2f2f6c6f63616c686f73742f63695f776572636865725f73797374656d2f75706c6f6164732f30303030352d412f383576627333636165773332312e6a7067, '00005-A', 'Bookeeper', '???', 'Á???', 'Á??', 'Á????', 'Female', '500', '2', '2', '?', '2019-12-04', '??', '??', 'Single', '50', '????', '????', '?????', '500000', '?', '2019-12-19', '???', '??', '2019-12-12', '???', '?', '2019-12-28', '', '', '', '', '', '', NULL, 'Applicant', NULL, NULL, NULL, NULL),
+(6, 0x68747470733a2f2f6c6f63616c686f73742f63695f776572636865725f73797374656d2f75706c6f6164732f30303030362d412f526162626974732b666f6f742b69732b737570706f7365642b746f2b62652b6c75636b792b5f36396661363033323666313237303266336135663439653766626430333239312e6a7067, '00006-A', 'Office Staff', '1111111', 'Aaa', 'Aaaaaaa', 'Aaaaaa', 'Male', '11', '11', '11', '11', '2019-12-12', '11', '12313', 'Single', '13', '123213', '32131', '32133', '1313', '123213', '2019-12-26', '1231', '1131313123', '2019-12-13', '12313', '13213123123', '2019-12-21', '', '', '', '', '', '', NULL, 'Expired', '1', '', '2020-05-02 04:15:45 PM', '2019-12-02 09:09:59 AM'),
+(7, 0x68747470733a2f2f6c6f63616c686f73742f63695f776572636865725f73797374656d2f75706c6f6164732f30303030372d412f6368726f6d655f567737685757644b62342e6a7067, '00007-A', 'Secretary', '90000', 'Arató', 'András', 'A', 'Male', '50', '5', '80', '111111', '2019-12-03', 'Hungary', 'z315135', 'Single', '3', 'K?szeg, Hungary', 'K?szeg, Hungary', '11111', '35735723542', '2524622753', '2019-12-31', '252462624624234', 'K?szeg', '2019-12-04', '26245234226', 'K?szeg', '2019-12-14', '', '', '', '', '', '', NULL, 'Applicant', NULL, NULL, NULL, '2019-12-02 09:34:26 AM'),
+(8, 0x687474703a2f2f6c6f63616c686f73742f63695f776572636865725f73797374656d2f75706c6f6164732f30303030382d412f31363630323832305f3136313233333230343338303238385f333934343238313336383536353532393633365f6e5f2d5f436f70792e6a7067, '00008-A', 'Office Staff', '20000', 'Last Name', 'First Name', 'Middle Initial', 'Male', '1', '1', '1', 'SAMPLE', '2019-12-11', 'SAMPLE', 'SAMPLE', 'Single', '1', 'SAMPLE', 'SAMPLE', 'SAMPLE', '1', 'SAMPLE', '2019-12-11', 'SAMPLE', 'SAMPLE', '2019-12-11', 'SAMPLE', 'SAMPLE', '2019-12-11', 'SAMPLE', 'SAMPLE', '2019-12-11', 'SAMPLE', 'SAMPLE', '2019-12-17', 'SAMPLE', 'Applicant', NULL, NULL, NULL, '2019-12-10 06:45:27 AM');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `audit_log`
+--
+
+CREATE TABLE `audit_log` (
+  `LogID` int(11) NOT NULL,
+  `LogType` varchar(255) DEFAULT NULL,
+  `UserAgent` varchar(255) DEFAULT NULL,
+  `IPAddress` varchar(255) DEFAULT NULL,
+  `Action` varchar(255) DEFAULT NULL,
+  `Time` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `beneficiaries`
+--
+
+CREATE TABLE `beneficiaries` (
+  `No` int(11) NOT NULL,
+  `ApplicationID` varchar(255) DEFAULT NULL,
+  `Name` varchar(255) DEFAULT NULL,
+  `Relationship` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clients`
+--
+
+CREATE TABLE `clients` (
+  `ClientID` int(11) NOT NULL,
+  `Name` varchar(255) DEFAULT NULL,
+  `Address` varchar(255) DEFAULT NULL,
+  `ContactNumber` varchar(255) DEFAULT NULL,
+  `Status` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `clients`
+--
+
+INSERT INTO `clients` (`ClientID`, `Name`, `Address`, `ContactNumber`, `Status`) VALUES
+(1, 'Some Guy', '123456789', '11111111111', 'Active'),
+(2, 'HELLO', '1', '1', 'Active'),
+(3, '5', '5', '5', 'Deleted');
 
 -- --------------------------------------------------------
 
@@ -65,9 +196,8 @@ INSERT INTO `admin` (`ID`, `Access_ID`, `Branch`) VALUES
 -- Table structure for table `employee`
 --
 
-DROP TABLE IF EXISTS `employee`;
-CREATE TABLE IF NOT EXISTS `employee` (
-  `Employee_No` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `employee` (
+  `Employee_No` int(11) NOT NULL,
   `Employee_ID` varchar(255) NOT NULL,
   `EmployeeImage` blob,
   `EmploymentType` varchar(255) DEFAULT NULL,
@@ -87,16 +217,196 @@ CREATE TABLE IF NOT EXISTS `employee` (
   `ATM` varchar(255) DEFAULT NULL,
   `DateHired` varchar(255) DEFAULT NULL,
   `DateSeparated` varchar(255) DEFAULT NULL,
-  `Status` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`Employee_No`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `Status` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `employee`
+-- Table structure for table `employment_record`
 --
 
-INSERT INTO `employee` (`Employee_No`, `Employee_ID`, `EmployeeImage`, `EmploymentType`, `LastName`, `FirstName`, `MiddleInitial`, `Address`, `BirthDate`, `BirthPlace`, `Gender`, `MaritalStatus`, `ProjectAssigned`, `SSS`, `Philhealth`, `HDMF`, `TIN`, `ATM`, `DateHired`, `DateSeparated`, `Status`) VALUES
-(1, '00001-A', 0x68747470733a2f2f6c6f63616c6465762e746573742f48525f53797374656d2f75706c6f6164732f30303030312d412f31343062313465303739646338356337326339663032356362363764623536322e706e67, 'Employee', 'Dasdas', 'Dasdasd', 'Asdas', 'asdasdasdas', '2019-10-21', 'asdasdas', 'Female', 'Single', 'dasdada', 'sdadada', NULL, NULL, NULL, NULL, '2019-10-23', '2019-10-31', 'Active');
+CREATE TABLE `employment_record` (
+  `No` int(11) NOT NULL,
+  `ApplicantID` varchar(255) DEFAULT NULL,
+  `Name` varchar(255) DEFAULT NULL,
+  `Address` varchar(255) DEFAULT NULL,
+  `PeriodCovered` varchar(255) DEFAULT NULL,
+  `Position` varchar(255) DEFAULT NULL,
+  `Salary` varchar(255) DEFAULT NULL,
+  `CauseOfSeparation` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `employment_record`
+--
+
+INSERT INTO `employment_record` (`No`, `ApplicantID`, `Name`, `Address`, `PeriodCovered`, `Position`, `Salary`, `CauseOfSeparation`) VALUES
+(1, '00007-A', 'Some Dude', 'Somewhere in Russia', '2010', 'Manager', '75000', 'Late by 1 minute'),
+(2, '00008-A', 'SAMPLE', 'SAMPLE', 'SAMPLE', 'SAMPLE', 'SAMPLE', 'SAMPLE');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `machine_operated`
+--
+
+CREATE TABLE `machine_operated` (
+  `No` int(11) NOT NULL,
+  `ApplicantID` varchar(255) DEFAULT NULL,
+  `MachineName` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `machine_operated`
+--
+
+INSERT INTO `machine_operated` (`No`, `ApplicantID`, `MachineName`) VALUES
+(1, '00007-A', 'HCHCHCHC2000'),
+(2, '00007-A', '50002xAS'),
+(3, '00008-A', 'SAMPLE');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `relatives`
+--
+
+CREATE TABLE `relatives` (
+  `No` int(11) NOT NULL,
+  `ApplicantID` varchar(255) DEFAULT NULL,
+  `Relation` varchar(255) DEFAULT NULL,
+  `Name` varchar(255) DEFAULT NULL,
+  `Occupation` varchar(2555) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `acad_history`
+--
+ALTER TABLE `acad_history`
+  ADD PRIMARY KEY (`Acad_No`);
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`AdminNo`);
+
+--
+-- Indexes for table `applicants`
+--
+ALTER TABLE `applicants`
+  ADD PRIMARY KEY (`ApplicantNo`);
+
+--
+-- Indexes for table `audit_log`
+--
+ALTER TABLE `audit_log`
+  ADD PRIMARY KEY (`LogID`);
+
+--
+-- Indexes for table `beneficiaries`
+--
+ALTER TABLE `beneficiaries`
+  ADD PRIMARY KEY (`No`);
+
+--
+-- Indexes for table `clients`
+--
+ALTER TABLE `clients`
+  ADD PRIMARY KEY (`ClientID`);
+
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`Employee_No`);
+
+--
+-- Indexes for table `employment_record`
+--
+ALTER TABLE `employment_record`
+  ADD PRIMARY KEY (`No`);
+
+--
+-- Indexes for table `machine_operated`
+--
+ALTER TABLE `machine_operated`
+  ADD PRIMARY KEY (`No`);
+
+--
+-- Indexes for table `relatives`
+--
+ALTER TABLE `relatives`
+  ADD PRIMARY KEY (`No`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `acad_history`
+--
+ALTER TABLE `acad_history`
+  MODIFY `Acad_No` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `AdminNo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `applicants`
+--
+ALTER TABLE `applicants`
+  MODIFY `ApplicantNo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `audit_log`
+--
+ALTER TABLE `audit_log`
+  MODIFY `LogID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `beneficiaries`
+--
+ALTER TABLE `beneficiaries`
+  MODIFY `No` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `clients`
+--
+ALTER TABLE `clients`
+  MODIFY `ClientID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `Employee_No` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `employment_record`
+--
+ALTER TABLE `employment_record`
+  MODIFY `No` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `machine_operated`
+--
+ALTER TABLE `machine_operated`
+  MODIFY `No` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `relatives`
+--
+ALTER TABLE `relatives`
+  MODIFY `No` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
